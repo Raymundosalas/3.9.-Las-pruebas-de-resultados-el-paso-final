5334fe0cfea3f04e52c99226a3e658efce636bc6
Raymundosalas <128091595+Raymundosalas@users.noreply.github.com>
2026-04-20 09:08:05 -0600

    Implement PlayerController for character movement

---
+++ PlayerController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            GameManager.instance.GameOver();
+        }
+    }
+}
